1d770d11227a3588e56a0ff2111b4ba80d71da0a
William <William@MacBook-Pro-2.local>
2018-05-24 00:16:20 -0700

    make word.js functional

--- letter.js
+++ letter.js
@@ -1,33 +1,33 @@
 
 // create a constructor for each word thats gonna be letterGuessed
-var letter = function (letter) {
-    this.letter = letter;
+var Letter = function (letter) {
+    this.underlyingLetter = letter;
+    
     //A boolean value that stores whether that letter has been letterGuessed yet
     this.letterGuessed = false;
     //A function that returns the underlying character if the letter has been letterGuessed, or a placeholder (like an underscore) if the letter has not been letterGuessed
-    this.display = function (letter) {
+    this.display = function () {
         
         if (this.letterGuessed) {
-            console.log(this.letter);
-            this.letterGuessed = true;
+            return this.underlyingLetter;
         }
-        else if (letter == " ") {
-            this.letter = "Y";
+        else if (this.underlyingLetter == " ") {
+            return " ";
         }
         else {
-            this.letter = "_";
+            return "_";
         }
         
     }
     // compare user input with the word 
-    this.checkletter = function (userInput) {
-        if(userInput === this.letter) {
+    this.checkLetter = function (userInput) {
+        if(userInput === this.underlyingLetter) {
             this.letterGuessed = true;
         }
     }   
 }
 
-module.exports = letter;
+module.exports = Letter;
 
 // var a="word";
 // console.log (a.length); // return 4
--- word.js
+++ word.js
@@ -1,23 +1,45 @@
 var letter = require("./letter.js");
 
-var word = function () {
-    this.letterArr = [];
-    this.newWord = function (word) {
-        var wordArry = word.split("");
-        var display = [];
-        for (let i = 0; i <wordArry.length; i++) {
-            var wordChar = new letter(wordArry[i])
-            this.letterArr.push(wordChar);
-            wordChar.display(wordArry[i]);
-        }
+var Word = function (word) {
+    //word after guess
+    this.runningWord = [];
+    // to hold lettes in new word
+    this.wordArry = word.split("");
+    //to hold new obj for earch letter in new word
+    this.letterObjArr = [];
+    this.letterInWord = function () {
         
-        for (let j=0; j<this.letterArr.length; j++) {
-            display.push(this.letterArr[0].letter);
+        // loop through arry of and create object of each letter in word
+        for (let i = 0; i <this.wordArry.length; i++) {
+            var wordChar = new letter(this.wordArry[i])
+            this.letterObjArr.push(wordChar);
+        };
+    };
+    this.printWord = function () {
+        //print underscore if no match, letter if there is a match
+        for (let j=0; j<this.letterObjArr.length; j++) {
+           
+            this.runningWord.push(this.letterObjArr[j].display());
         }
-        console.log(wordArry);
-        console.log(display.join(" "));
-    }   
+        console.log(this.runningWord.join(" "));
+    };
+
+    this.getLetter = function (input) {
+        var number; // to store i when matches
+        // compare user input with characters in new word 
+        for (let i=0; i<this.letterObjArr.length; i++) {
+
+            this.letterObjArr[i].checkLetter(input);
+            this.letterObjArr[i].display();
+        }
+        this.runningWord=[];
+        this.printWord();
+    }
+
 }   
 
-var wordpress = new word("word press");
-wordpress.newWord("word press");
+var wordpress = new Word("word press");
+wordpress.letterInWord();
+wordpress.printWord();
+wordpress.getLetter("s");
+// wordpress.getLetter("r");
