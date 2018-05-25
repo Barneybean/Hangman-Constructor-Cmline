ccc8892ce417a229b88de9a56b04a8e76eb7f425
William <William@MacBook-Pro-2.local>
2018-05-25 00:06:01 -0700

    complete basic function

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var guessRight = 0;
 var guessWrong = 0;
 
 //add word for questions 
-var wordList = ["mountain", "chameleon", "salmon", "grayling"];
+var wordList = ["mountain", "chameleon"];
 //to create a arry that hold on the word obj to be gueseed
 var wordObjList = [];
 
@@ -21,58 +21,93 @@ for(let i = 0; i < wordList.length; i++) {
 }
 // console.log(wordObjList);
 
-var askQUestion = function () {
+var askQuestion = function () {
     var currentRound = round+1;
-    
+    var letterGuessed = [];
     if (guessCount >0) {
         // console.log(wordObjList[round]),
         // to print underscore of letter
         wordObjList[round].printWord();
         //get user inputs and check letter
         inquirer.prompt ([
-        {
-            type: "input",
-            name: "userGuess",
-            message: "Round " + currentRound+ "; " +guessCount + " guess left; Guess a letter: " + "\n",
-            // validate: allow only one letter
-        },
-    ]).then(function(guess){
+            {
+                type: "input",
+                name: "userGuess",
+                message: "Round " + currentRound+ "; " +guessCount + " guess left; Guess a letter: " + "\n",
+                validate: function validateGuess(userGuess) {
+                    if (userGuess.length>1) {
+                        console.log('\nEnter 1 letter only\n');
+                        return;
+                    } else if (!userGuess.match(/^[a-zA-Z]*$/)) {
+                        console.log("\nThat's not a letter! try again...\n");
+                        return
+                    } else {
+                        return true;
+                    }
+                }
+            }
+        ]).then(function(guess){
+            //to prevent gussing same number
+            if (letterGuessed.indexOf(guess.userGuess) === -1) {
+                
+                letterGuessed.push(guess.userGuess);
+                // console.log(guessCount + " guess left");
+                //guess chance -1
+                guessCount--;
+                
+                //if there is word not guessed then continue game
+                if (round <= wordList.length-1) {
+                    wordObjList[round].getLetter(String(guess.userGuess));
+                }
+                //end of all rounds
+                else {
+                    console.log("Game Over; You guessed: " + guessRight + " correctly and " +guessWrong + " wrong.\n")
+                    inquirer.prompt([
+                        {
+                            type: "list",
+                            name: "restart",
+                            message: "restart game?",
+                            choices: ["Yes","No"]
+                        }
+                    ]).then(function (answer) {
+                        if (answer.choices == "Yes") {
+                            restartGame();
+                        } else{
+                            console.log("To End Game, Exit Terminal ..");
+                        }
+                    })
+                    return;
+                };
+                // guess the whole word before chances run out, guess right ++
+        
+                if (guessCount >= 0 && wordObjList[round].runningWord.join("") === wordObjList[round].wordArry.join("")) {
+                    console.log("***good job, next word\n");
+                    guessRight++;
+                    round++;
+                    guessCount = 15;
+                }
+                // fail to guess the whole word before chances run out, guess wrong ++
+                else if(guessCount === 0 && wordObjList[round].runningWord.join("") !== wordObjList[round].wordArry.join("")) {
+                    console.log("***you lose, next word\n");
+                    guessWrong++;
+                    round++;
+                    guessCount = 15
+                }
+            
+                //recursion####### to allow next guess
+                askQuestion();
+            
+            } else {
+                console.log("Letter already guessed, try another one..");
+            }
 
-        // console.log(guessCount + " guess left");
-        //guess chance -1
-        guessCount--;
-        
-        //if there is word not guessed then continue game
-        if (round < wordList.length) {
-            wordObjList[round].getLetter(String(guess.userGuess));
-        
-        }
-        //end of all rounds
-        else {
-            console.log("Game Over +\n + you guessed: " + guessRight + " correctly and " +guessWrong + " wrong. +\n + Restart Game? ")
-        };
-        // guess the whole word before chances run out, guess right ++
-       
-        if (guessCount >= 0 && wordObjList[round].runningWord.join("") === wordObjList[round].wordArry.join("")) {
-            console.log("***good job, next word\n");
-            guessRight++;
-            round++;
-            guessCount = 15;
-        }
-        // fail to guess the whole word before chances run out, guess wrong ++
-        else if(guessCount === 0 && wordObjList[round].runningWord.join("") !== wordObjList[round].wordArry.join("")) {
-            console.log("***you lose, next word\n");
-            guessWrong++;
-            round++;
-            guessCount = 15
-        }
-        else{console.log(6);}
-        
-        //recursion####### to allow next guess
-        askQUestion();
-    })
-    }
-    
-    
+            
+        });
+    };
 };
-askQUestion();
+askQuestion();
+
+function restartGame () {
+    //reset all values;
+    console.log("coming soon");
+};
